366cafdc62c17928f820cac07a2dd44ea7d8944a
vigneshnadar <nadar.v@husky.neu.edu>
2016-12-12 15:09:24 -0400

    added ejs

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ import config from './config';
 import express from 'express';
 const server=express();
 
+server.set('viewengine','ejs');
+
 server.get('/',(req,res) => {
-res.send('Hello World Bask Kya');
+//res.send('Hello World Bask Kya');
+res.render('index.ejs');
 });
 
 
-server.get('/about.html',(req,res) => {
-res.send('The About Page');
-});
+server.use(express.static('public'));
+
+// server.get('/about.html',(req,res) => {
+// res.send('The About Page');
+// });
 
 server.listen(config.port,() => {
 	console.log('express is listening on port: ',config.port);
